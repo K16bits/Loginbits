bf9f548f5e0232a9ad44c91034be2fe83f7d0a0d
K16bits <52179531+K16bits@users.noreply.github.com>
2020-09-15 16:07:56 -0300

    novas rotas

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter,Route,Switch} from 'react-router-dom'
 
 import Home from "./pages/Home"
 import Profile from "./pages/Profile"
+import Subscribe from "./pages/Subscribe"
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home}/>
+                <Route path="/subscribe" component={Subscribe} />
                 <Route path="/profile" component={Profile} />
             </Switch>
         </BrowserRouter>
